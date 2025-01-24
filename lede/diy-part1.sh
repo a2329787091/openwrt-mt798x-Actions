609c92e0da8ba505af9fdf7e76a463dfb9180eef
a2329787091 <129927464+a2329787091@users.noreply.github.com>
2025-01-24 14:13:51 +0800

    Update diy-part1.sh

--- lede/diy-part1.sh
+++ lede/diy-part1.sh
@@ -3,3 +3,4 @@
 # Add a feed source
 #echo 'src-git moruiris https://github.com/moruiris/openwrt-packages;lede' >>feeds.conf.default
 git clone -b lede https://github.com/moruiris/openwrt-packages ./package/openwrt-packages
+echo 'src-git smpackage https://github.com/kenzok8/small-package' >>feeds.conf.default
